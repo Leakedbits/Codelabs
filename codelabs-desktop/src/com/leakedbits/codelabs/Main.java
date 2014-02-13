fc89a180869e97844238e6bdba2932fdca2219e0
David Moreno García <david.mogar@gmail.com>
2014-02-13 16:42:52 +0100

    Adds a bouncing ball box2D sample

--- codelabs-desktop/src/com/leakedbits/codelabs/Main.java
+++ codelabs-desktop/src/com/leakedbits/codelabs/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "codelabs";
+		cfg.title = Codelabs.TITLE;
 		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
