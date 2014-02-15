2dc83e509b6521973f56d1728b9dab838da3ddfe
David Moreno García <david.mogar@gmail.com>
2014-02-15 13:28:34 +0100

    Remove auto generated comments

--- codelabs/src/com/leakedbits/codelabs/box2d/SpawnBodiesTest.java
+++ codelabs/src/com/leakedbits/codelabs/box2d/SpawnBodiesTest.java
@@ -192,19 +192,16 @@ public class SpawnBodiesTest extends Test implements InputProcessor {
 
 	@Override
 	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -223,19 +220,16 @@ public class SpawnBodiesTest extends Test implements InputProcessor {
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
