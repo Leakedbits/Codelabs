d01f93a370b752896ed3bfcfc2e187ef066466d8
David Moreno García <david.mogar@gmail.com>
2014-02-15 15:26:57 +0100

    Add new test (DragAndDropTest) to Box2D collection
    This new test shows how to use MouseJoint to drag and drop bodies.

--- codelabs/src/com/leakedbits/codelabs/box2d/utils/Box2DTests.java
+++ codelabs/src/com/leakedbits/codelabs/box2d/utils/Box2DTests.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.leakedbits.codelabs.box2d.BouncingBallTest;
+import com.leakedbits.codelabs.box2d.DragAndDropTest;
 import com.leakedbits.codelabs.box2d.SpawnBodiesTest;
 import com.leakedbits.codelabs.utils.Test;
 
@@ -14,7 +15,8 @@ public class Box2DTests {
 	public static final List<Class<? extends Test>> tests = new ArrayList<Class<? extends Test>>(
 			Arrays.asList(
 					BouncingBallTest.class,
-					SpawnBodiesTest.class
+					SpawnBodiesTest.class,
+					DragAndDropTest.class
 			)
 	);
 
