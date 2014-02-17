fe5485d995ec2ca8db7855de3f3f801f69f0c3b6
David Moreno García <david.mogar@gmail.com>
2014-02-17 13:39:12 +0100

    Update version number to 0.3.0

--- codelabs/src/com/leakedbits/codelabs/Codelabs.java
+++ codelabs/src/com/leakedbits/codelabs/Codelabs.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Game;
 public class Codelabs extends Game {
 
 	public static final String TITLE = "Codelabs";
+	public static final String VERSION = "v0.3.0";
+	
 	public static final float TARGET_WIDTH = 800;
 			
 	@Override
