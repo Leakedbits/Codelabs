1903d35f45a4eb3db2706382c8183ee1b7b700e2
David Moreno García <david.mogar@gmail.com>
2014-02-20 16:25:33 +0100

    Refactor Box2DFactory
    Now three calls are necessary to create a body but would be easier for
    the final user to understand the code.

--- codelabs/src/com/leakedbits/codelabs/box2d/utils/Box2DFactory.java
+++ codelabs/src/com/leakedbits/codelabs/box2d/utils/Box2DFactory.java
@@ -8,203 +8,78 @@ import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Box2DFactory {
 
-	/**
-	 * Create a Box2D box and add it to the world.
-	 * 
-	 * @param world
-	 *            World where to add the new box
-	 * @param bodyType
-	 *            Type of body (Dynamic, Kinematic, Static)
-	 * @param position
-	 *            Position of the box
-	 * @param halfWidth
-	 *            Half of the width of the box
-	 * @param halfHeight
-	 *            Half of the height of the box
-	 * @param density
-	 *            Fixture density
-	 * @param friction
-	 *            Fixture friction
-	 * @param restitution
-	 *            Fixture restitution
-	 * @return A new Box2D body with box shape
-	 */
-	public static Body createBox(World world, BodyType bodyType,
-			Vector2 position, float halfWidth, float halfHeight, float density,
-			float friction, float restitution) {
-
-		/* Box body definition. Represents a single point in the world */
+	public static Body createBody(World world, BodyType bodyType,
+			FixtureDef fixtureDef, Vector2 position) {
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = bodyType;
 		bodyDef.position.set(position);
 
-		/* Shape definition (the actual shape of the body) */
-		PolygonShape boxShape = new PolygonShape();
-
-		/*
-		 * Use setAsBox to define the box shape. We have to specify half-width
-		 * and half-height to the method.
-		 */
-		boxShape.setAsBox(halfWidth, halfHeight);
-
-		/*
-		 * Fixture definition. Define properties of a body like the shape, the
-		 * density of the body, its friction or its restitution (how 'bouncy' a
-		 * fixture is) in a physics scene.
-		 */
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = boxShape;
-		fixtureDef.density = density;
-		fixtureDef.friction = friction;
-		fixtureDef.restitution = restitution;
-
-		/* Create body and fixture */
 		Body body = world.createBody(bodyDef);
 		body.createFixture(fixtureDef);
 
-		/* Dispose shape once the body is added to the world */
-		boxShape.dispose();
+		fixtureDef.shape.dispose();
 
 		return body;
 	}
 
-	/**
-	 * Create a Box2D circle and add it to the world.
-	 * 
-	 * @param world
-	 *            World where to add the new circle
-	 * @param bodyType
-	 *            Type of body (Dynamic, Kinematic, Static)
-	 * @param position
-	 *            Position of the circle
-	 * @param radius
-	 *            Radius of the circle
-	 * @param density
-	 *            Fixture density
-	 * @param friction
-	 *            Fixture friction
-	 * @param restitution
-	 *            Fixture restitution
-	 * @return A new Box2D body with circle shape
-	 */
-	public static Body createCircle(World world, BodyType bodyType,
-			Vector2 position, float radius, float density, float friction,
-			float restitution) {
+	public static Shape createBoxShape(float halfWidth, float halfHeight) {
+		PolygonShape boxShape = new PolygonShape();
+		boxShape.setAsBox(halfWidth, halfHeight);
 
-		/* Circle body definition. Represents a single point in the world */
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = bodyType;
-		bodyDef.position.set(position);
+		return boxShape;
+	}
 
-		/* Shape definition (the actual shape of the body) */
+	public static Shape createChainShape(Vector2[] vertices) {
+		ChainShape chainShape = new ChainShape();
+		chainShape.createChain(vertices);
+
+		return chainShape;
+	}
+
+	public static Shape createCircleShape(float radius) {
 		CircleShape circleShape = new CircleShape();
 		circleShape.setRadius(radius);
 
-		/*
-		 * Fixture definition. Define properties of a body like the shape, the
-		 * density of the body, its friction or its restitution (how 'bouncy' a
-		 * fixture is) in a physics scene.
-		 */
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = circleShape;
-		fixtureDef.density = 2.5f;
-		fixtureDef.friction = 0.25f;
-		fixtureDef.restitution = 0.75f;
-
-		/* Create body and fixture */
-		Body body = world.createBody(bodyDef);
-		body.createFixture(fixtureDef);
-
-		/* Dispose shape once the body is added to the world */
-		circleShape.dispose();
-
-		return body;
+		return circleShape;
 	}
 
-	/**
-	 * Create a Box2D triangle and add it to the world.
-	 * 
-	 * @param world
-	 *            World where to add the new triangle
-	 * @param bodyType
-	 *            Type of body (Dynamic, Kinematic, Static)
-	 * @param position
-	 *            Position of the triangle
-	 * @param halfBase
-	 *            Half of the base of the triangle
-	 * @param halfHeight
-	 *            Half of the height of the triangle
-	 * @param density
-	 *            Fixture density
-	 * @param friction
-	 *            Fixture friction
-	 * @param restitution
-	 *            Fixture restitution
-	 * @return A new Box2D body with triangle shape
-	 */
-	public static Body createTriangle(World world, BodyType bodyType,
-			Vector2 position, float halfBase, float halfHeight, float density,
-			float friction, float restitution) {
+	public static Shape createPolygonShape(Vector2[] vertices) {
+		PolygonShape polygonShape = new PolygonShape();
+		polygonShape.set(vertices);
 
-		/* Box body definition. Represents a single point in the world */
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = bodyType;
-		bodyDef.position.set(position);
+		return polygonShape;
+	}
 
-		/* Shape definition (the actual shape of the body) */
+	public static Shape createTriangleShape(float halfWidth, float halfHeight) {
 		PolygonShape triangleShape = new PolygonShape();
 		triangleShape
-				.set(new Vector2[] { new Vector2(-halfBase, -halfHeight),
+				.set(new Vector2[] { new Vector2(-halfWidth, -halfHeight),
 						new Vector2(0, halfHeight),
-						new Vector2(halfBase, -halfHeight) });
+						new Vector2(halfWidth, -halfHeight) });
 
-		/*
-		 * Fixture definition. Define properties of a body like the shape, the
-		 * density of the body, its friction or its restitution (how 'bouncy' a
-		 * fixture is) in a physics scene.
-		 */
+		return triangleShape;
+	}
+
+	public static FixtureDef createFixture(Shape shape, float density,
+			float friction, float restitution, boolean isSensor) {
 		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = triangleShape;
+		fixtureDef.shape = shape;
 		fixtureDef.density = density;
 		fixtureDef.friction = friction;
 		fixtureDef.restitution = restitution;
 
-		/* Create body and fixture */
-		Body body = world.createBody(bodyDef);
-		body.createFixture(fixtureDef);
-
-		/* Dispose shape once the body is added to the world */
-		triangleShape.dispose();
-
-		return body;
+		return fixtureDef;
 	}
 
-	/**
-	 * Create walls around the viewport to keep objects inside.
-	 * 
-	 * @param world
-	 *            World where to add the new walls
-	 * @param viewportWidth
-	 *            Viewport width
-	 * @param viewportHeight
-	 *            Viewport height
-	 * @param offsetX
-	 *            Horizontal distance from edge to wall position
-	 * @param offsetY
-	 *            Vertical distance from edge to wall position
-	 * @param density
-	 *            Fixture density
-	 * @return A new Box2D body
-	 */
 	public static Body createWalls(World world, float viewportWidth,
-			float viewportHeight, float offsetX, float offsetY, float density) {
-
-		float halfWidth = viewportWidth / 2 - offsetX;
-		float halfHeight = viewportHeight / 2 - offsetY;
+			float viewportHeight, float offset) {
+		float halfWidth = viewportWidth / 2 - offset;
+		float halfHeight = viewportHeight / 2 - offset;
 
 		Vector2[] vertices = new Vector2[] {
 				new Vector2(-halfWidth, -halfHeight),
@@ -213,60 +88,11 @@ public class Box2DFactory {
 				new Vector2(-halfWidth, halfHeight),
 				new Vector2(-halfWidth, -halfHeight) };
 
-		return createChain(world, BodyType.StaticBody, new Vector2(0, 0),
-				vertices, density, 0.5f, 0f);
+		Shape shape = createChainShape(vertices);
+		FixtureDef fixtureDef = createFixture(shape, 1, 0.5f, 0, false);
+
+		return createBody(world, BodyType.StaticBody, fixtureDef, new Vector2(
+				0, 0));
 	}
 
-	/**
-	 * Create a custom Box2D chain object.
-	 * 
-	 * @param world
-	 *            World where to add the new chain
-	 * @param bodyType
-	 *            Type of body (Dynamic, Kinematic, Static)
-	 * @param position
-	 *            Position of the chain
-	 * @param vertices
-	 *            Vertices of the chain
-	 * @param density
-	 *            Fixture density
-	 * @param friction
-	 *            Fixture friction
-	 * @param restitution
-	 *            Fixture restitution
-	 * @return A new Box2D chain
-	 */
-	public static Body createChain(World world, BodyType bodyType,
-			Vector2 position, Vector2[] vertices, float density,
-			float friction, float restitution) {
-
-		/* Chain body definition. Represents a single point in the world */
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = bodyType;
-		bodyDef.position.set(position);
-
-		/* Shape definition (the actual shape of the body) */
-		ChainShape chainShape = new ChainShape();
-		chainShape.createChain(vertices);
-
-		/*
-		 * Fixture definition. Define properties of a body like the shape, the
-		 * density of the body, its friction or its restitution (how 'bouncy' a
-		 * fixture is) in a physics scene.
-		 */
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = chainShape;
-		fixtureDef.density = density;
-		fixtureDef.friction = friction;
-		fixtureDef.restitution = restitution;
-
-		/* Create body and fixture */
-		Body body = world.createBody(bodyDef);
-		body.createFixture(fixtureDef);
-
-		/* Dispose shape once the body is added to the world */
-		chainShape.dispose();
-
-		return body;
-	}
 }
