668863d9be9f27eddfec3e6977531a890e528952
David Moreno García <david.mogar@gmail.com>
2014-02-23 21:59:51 +0100

    Change code version to 0.5.0

--- codelabs/src/com/leakedbits/codelabs/Codelabs.java
+++ codelabs/src/com/leakedbits/codelabs/Codelabs.java
@@ -5,14 +5,13 @@ import com.badlogic.gdx.Game;
 public class Codelabs extends Game {
 
 	public static final String TITLE = "Codelabs";
-	public static final String VERSION = "v0.4.0";
+	public static final String VERSION = "v0.5.0";
 	
 	public static final float TARGET_WIDTH = 800;
 			
 	@Override
 	public void create() {
 		setScreen(new SplashScreen());
-//		setScreen(new BuoyancySample());
 	}
 	
 }
