4f1e0b2f42f2e20a4c272ed425c82aed23008d16
David Moreno García <david.mogar@gmail.com>
2014-03-03 23:48:11 +0100

    Update code version to v0.6.0

--- codelabs/src/com/leakedbits/codelabs/Codelabs.java
+++ codelabs/src/com/leakedbits/codelabs/Codelabs.java
@@ -6,14 +6,13 @@ import com.leakedbits.codelabs.box2d.JumpingSample;
 public class Codelabs extends Game {
 
 	public static final String TITLE = "Codelabs";
-	public static final String VERSION = "v0.5.0";
+	public static final String VERSION = "v0.6.0";
 	
 	public static final float TARGET_WIDTH = 800;
 			
 	@Override
 	public void create() {
-//		setScreen(new SplashScreen());
-		setScreen(new JumpingSample());
+		setScreen(new SplashScreen());
 	}
 	
 }
