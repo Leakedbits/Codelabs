946d3e6b53afec3c0b840151b18625e118469cef
David Moreno García <david.mogar@gmail.com>
2014-03-06 12:54:54 +0100

    Remove unused class

--- codelabs/src/com/leakedbits/codelabs/libgdx/ParticlesSample.java
+++ codelabs/src/com/leakedbits/codelabs/libgdx/ParticlesSample.java
@@ -9,8 +9,9 @@ import com.leakedbits.codelabs.utils.Sample;
 public class ParticlesSample extends Sample {
 
 	private SpriteBatch batch;
+
 	private ParticleEffect particle;
-	
+
 	/**
 	 * Main constructor used to update sample name.
 	 */
@@ -23,24 +24,23 @@ public class ParticlesSample extends Sample {
 	 * drag bodies.
 	 */
 
-	
 	@Override
 	public void render(float delta) {
 		/* Clear screen with a black background */
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.begin();
 		particle.draw(batch, delta);
 		batch.end();
 	}
-	
+
 	@Override
 	public void show() {
 		super.show();
-		
+
 		batch = new SpriteBatch();
-		
+
 		/*
 		 * Create the particle effect loading the internal file from assets
 		 * folder
@@ -48,7 +48,8 @@ public class ParticlesSample extends Sample {
 		particle = new ParticleEffect();
 		particle.load(Gdx.files.internal("data/particles/fire.p"),
 				Gdx.files.internal("data/images/particles"));
-		particle.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		particle.setPosition(Gdx.graphics.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2);
 		particle.start();
 	}
 
@@ -57,18 +58,18 @@ public class ParticlesSample extends Sample {
 		batch.dispose();
 		particle.dispose();
 	}
-	
+
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		particle.setPosition(screenX, Gdx.graphics.getHeight() - screenY);
 
 		return true;
 	}
-	
+
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		particle.reset();
-		
+
 		return true;
 	}
-	
+
 }
